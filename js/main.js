6e87437556d544fdf6add8e9c26e9242b1a3ceba
AWhiteRabbit <doncameron09@gmail.com>
2018-04-13 05:33:30 -0400

    entries added no styles yet

--- js/main.js
+++ js/main.js
@@ -1,3 +1,25 @@
-$(".submit").on("click", function () {
-    let ws = ($("#ws").val());
+// https://en.wikipedia.org/w/api.php?action=opensearch&format=json&search=balls
+//document.querySelector(".btn").addEventListener("click", x => {
+
+
+document.querySelector(".btn").addEventListener("click", x => {
+    let searchText = document.querySelector("input[name='text']").value;
+    let urlJSON = "https://en.wikipedia.org//w/api.php?action=opensearch&format=json&origin=*&search="+searchText;
+fetch(urlJSON).then(response => {
+
+    return response.json();   
+}).then( j => {
+
+
+    for (x in j[1]) {
+        let openSearch = j[1][x];
+        let openInfo = j[2][x];
+        let openLink = j[3][x];
+        let entry = "<section><h2>"+openSearch+"</h2><p>"+openInfo+"</p><a>"+openLink+"</a></section>";
+
+        document.querySelector(".result").insertAdjacentHTML('beforeend', entry);
+    }
+}).catch(err => {
+    console.log(err);
 })
+});
